3f4fee905d5fff491c8d96c4fd33c867df6ad6f8
Aaron C. <81132763+ACr0ss@users.noreply.github.com>
2025-02-05 20:54:33 +0100

    Migrated from kapt to ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
 }
 
@@ -48,18 +48,12 @@ android {
 }
 
 dependencies {
-    // Core Libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.material)
-
-    // Lifecycle
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
-
-    // Jetpack Compose
     implementation(platform(libs.compose.bom))
-
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
@@ -67,27 +61,16 @@ dependencies {
     implementation(libs.compose.foundation)
     implementation(libs.compose.runtime)
     implementation(libs.compose.runtime.livedata)
-
     debugImplementation(libs.compose.ui.tooling)
     debugImplementation(libs.compose.ui.test.manifest)
-
-    // Navigation
     implementation(libs.navigation.compose)
-
-    // DataStore
     implementation(libs.datastore.preferences)
-
-    // Dagger-Hilt
     implementation(libs.dagger.hilt.android)
-    kapt(libs.dagger.hilt.compiler)
+    ksp(libs.dagger.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
-
-    // Retrofit and Networking
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.gson)
-
-    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
